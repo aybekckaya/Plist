3ec8c7a2f5efd4e289678c9d7af84bc8249ce2a1
aybek can kaya <aybekcankaya@icloud.com>
2017-09-10 16:14:29 +0300

    Plist with unit tests

--- Plist/Plist/Plist/Plist.swift
+++ Plist/Plist/Plist/Plist.swift
@@ -8,65 +8,139 @@
 
 import UIKit
 
-/**
- *** Specs
- ** Bundle içerisinden bir plisti okuma ve ilk okumada folderpath içerisine aynı isim ile kaydetme.
- 
- *** constants :
- **  folderPath // plist dosyalarının bulunacağı yer. Eğer böyle bir dosya yeri yok ise ilk save yapıldığında herhangi bir plist burası yaratılır.
- 
- *** variables:
- enum SourcePath{ case documentsDirectory , case bundle }
- 
- *** init(name:String , source:SourcePath = .documentsDir) : name ile verilen dosya yolunda plist aranır . Eğer bulunamazsa yeni yaratılır ilk save veya read yapıldığı anda.
- Eğer sourcePath bundle ise documentsDir içinde bu plist aranır. Eğer bulunmazsa bundle içinden plist okunur ve içindekiler documents içine kaydedilir.
- 
- *** save(val:Any , key:String , completion:()->Void) : plist içerisine async şekilde content json a dönüştürülerek yazılır. Yani varolan contente append yapılır. jsona çevrim yapılamazsa error handling olacak.
- 
- *** delete(key:String , completion:()->Void? = nil ) : plist içerisinden ilgili key async şekilde silinir.
- 
- *** removePlist() : plist tümüyle silinir.
- 
- *** read(key:String , completion:(dctContent:[String:Any]? , error:Error?)->Void) : ilgili key okunur ve sonuçlar dctContents içerisinde döndürülür. dct content -> key(parametre olarak verilen key) , value:(json parsed value type Any )
- 
- *** readAll(completion:(dctContent:[String:Any]? , error:Error?)->Void) : Plist içerisindeki tüm data döndürülür.
- 
- *** static allLists()->[Plist] : folder içerisindeki tüm listeleri döndürür.
-
- */
-
-
 enum PlistSourcePath {
     case documentsDirectory
     case bundle
 }
 
-class Plist: NSObject {
 
+class Plist: NSObject {
+    enum PlistError:Error {
+        case couldNotFetchPlistContents
+        case couldNotConvertToJsonString
+        case couldNotDecodeJson
+        
+        func errorComponent()->Error {
+            switch self {
+            case .couldNotConvertToJsonString:
+                return NSError(domain: "could not convert json", code: 10000, userInfo: nil) as Error
+            case .couldNotFetchPlistContents:
+                return NSError(domain: "could not fetch plist contents", code: 10001, userInfo: nil) as Error
+            case .couldNotDecodeJson:
+                return NSError(domain: "could not decode json string", code: 10002, userInfo: nil) as Error
+            }
+        }
+        
+    }
+    
     fileprivate var name:String = ""
     fileprivate var source:PlistSourcePath = .documentsDirectory
-    fileprivate var folderPath:String = "/plists/"
+    fileprivate var folderPath:String = "plists"
     
-    init(_name:String , _source:PlistSourcePath = .documentsDirectory , _folderPath:String = "/plists/") {
+    var plistPath:String {
+         get{ return plistFullPath() }
+    }
+    
+    init(_name:String , _source:PlistSourcePath = .documentsDirectory , _folderPath:String = "plists") {
         super.init()
         name = _name
         source = _source
-        folderPath = _folderPath
+        folderPath = _folderPath.replacingOccurrences(of: "/", with: "")
         source == .documentsDirectory ? createPlist() : copyFromBundle()
     }
     
     private func createPlist() {
-       /*
-        let fileManager = FileManager.default
-        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
-        let path = documentDirectory.appending("/profile.plist")
-         */
-        
-        
+        let fullPath:String = plistFullPath()
+        guard !fullPath.fileExistAtPath() else { return }
+        folderPath.createDir()
+        let emptyDct = NSDictionary(dictionary: [:])
+        emptyDct.write(toFile: fullPath, atomically: true)
  }
     
+    private func plistFullPath()->String {
+        return String.documentsDirectoryPath()+"/"+folderPath+"/"+name+".plist"
+    }
+    
     private func copyFromBundle() {
+        let fullname = name+".plist"
+        guard !plistPath.fileExistAtPath() else { return }
+        folderPath.createDir()
+        guard let documentsURL = Bundle.main.resourceURL?.appendingPathComponent(fullname) else { return }
+        do {
+            try FileManager.default.copyItem(atPath: (documentsURL.path), toPath: plistPath)
+        } catch let error as NSError {
+            print("Error:\(error.description)")
+        }
         
+    }
+    
+    fileprivate func contents()->[String:Any]? {
+        let path = plistFullPath()
+        guard let dct = NSDictionary(contentsOfFile: path) as? [String: Any] else { return nil }
+        return dct
+    }
+    
+    func save(key:String , data:Any , completion:@escaping (_ error:Error?)->Void) {
+        guard var contentsList = contents() else {
+            completion(PlistError.couldNotFetchPlistContents.errorComponent())
+            return
+        }
+        let dct:[String:Any] = [key:data]
+        guard let jsonStr = dct.toJsonString() else {
+            completion(PlistError.couldNotConvertToJsonString.errorComponent())
+            return
+        }
+        contentsList[key] = jsonStr
+        DispatchQueue.global(qos: .background).async {
+            (contentsList as NSDictionary).write(toFile: self.plistFullPath(), atomically: true)
+            DispatchQueue.main.async {
+               completion(nil)
+            }
+        }
+       
+    }
+    
+    func read(key:String , completion:@escaping(_ data:[String:Any]? , _ error:Error?)->Void) {
+        
+        DispatchQueue.global(qos: .background).async {
+            let path = self.plistFullPath()
+            guard let dct = NSDictionary(contentsOfFile: path) as? [String: Any] else {
+                DispatchQueue.main.async {
+                    completion(nil, PlistError.couldNotFetchPlistContents.errorComponent())
+                }
+                return
+            }
+            guard let content = dct[key] as? String else {
+                DispatchQueue.main.async {
+                    completion(nil, nil)
+                }
+                return
+            }
+            
+            guard let json = content.decodeJSON() else {
+                completion(nil, PlistError.couldNotDecodeJson.errorComponent())
+                return
+                
+            }
+            completion(json, nil)
+        }
+    
+    }
+    
+
+    func removePlist() {
+      
+        let fullPath:String = plistFullPath()
+        guard fullPath.fileExistAtPath() else { return }
+        fullPath.removeFile()
+    }
+    
+    static func allPlistFiles(directoryName:String)->[String] {
+        let allFiles:[String] = directoryName.allFilesInDirectory()
+        return allFiles.map{ $0.components(separatedBy: "/").last! }.filter{element in
+            let ext = element.components(separatedBy: ".").last!
+            return ext == "plist"
+            }.map{ $0.components(separatedBy: ".").first! }
     }
     
 }
@@ -90,17 +164,81 @@ extension JsonConvertible {
 extension Array:JsonConvertible {}
 extension Dictionary:JsonConvertible {}
 
-
-
 extension String {
+    
+    func decodeJSON()->[String:Any]? {
+        guard let data = self.data(using: String.Encoding.utf8) else { return nil }
+        do{
+            let jsonParsed = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+            return jsonParsed as? [String:Any]
+        }catch {
+            print("err: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
     static func documentsDirectoryPath()->String {
         return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
     }
     
-    
     func fileExistAtPath()->Bool {
         return FileManager.default.fileExists(atPath: self)
     }
+    
+    func directoryExists()->Bool {
+        let fileManager = FileManager.default
+        var isDir : ObjCBool = false
+        if fileManager.fileExists(atPath: self, isDirectory:&isDir) {
+            if isDir.boolValue {
+                // file exists and is a directory
+                return true
+            } else {
+                // file exists and is not a directory
+                fatalError("String:\(self) is not a directory path")
+            }
+        }
+      return false
+    }
+    
+   
+    
+    func removeFile() {
+        do {
+           try FileManager.default.removeItem(atPath: self)
+        }catch {
+            print("error : \(error.localizedDescription)")
+        }
+    }
+    
+    
+    func allFilesInDirectory()->[String] {
+       
+         var documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+         documentsUrl.appendPathComponent(self)
+        var contents:[String] = []
+        do {
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsUrl, includingPropertiesForKeys: nil, options: [])
+            contents = directoryContents.map{ $0.absoluteString }
+            return contents
+        } catch  {
+            return contents
+        }
+    }
+    
+    
+    func createDir() {
+        guard !self.directoryExists() else { return }
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let pt = self+"/"
+        let dataPath = documentsDirectory.appendingPathComponent(pt)
+        
+        do {
+            try FileManager.default.createDirectory(at: dataPath, withIntermediateDirectories: false, attributes: nil)
+        } catch let error as NSError {
+            print("Error creating directory: \(error.localizedDescription)")
+        }
+    }
+    
 }
 
 
--- Plist/PlistTests/PlistTests.swift
+++ Plist/PlistTests/PlistTests.swift
@@ -11,6 +11,10 @@ import XCTest
 
 class PlistTests: XCTestCase {
     
+    fileprivate let sampleListName:String = "sampleList"
+    fileprivate let sampleFolderName:String = "sampleFolder"
+    fileprivate let sampleListNameBundle:String = "sampleListFromBundle"
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,12 +23,77 @@ class PlistTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+       // let list = Plist(_name: sampleListName , _source: .documentsDirectory, _folderPath:sampleFolderName)
+        //list.removePlist()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCreatePlist() {
+        let _ = Plist(_name: sampleListName , _source: .documentsDirectory, _folderPath:sampleFolderName)
+        let files:[String] = sampleFolderName.allFilesInDirectory().map{
+            $0.components(separatedBy: "/").last!
+            }.map{
+                $0.components(separatedBy: ".").first!
+        }
+        XCTAssert(files.contains(sampleListName))
     }
+    
+    func testInitializePlistFromName() {
+        
+        let list = Plist(_name: sampleListName, _source: .documentsDirectory, _folderPath: sampleFolderName)
+        let allFiles:[String] = Plist.allPlistFiles(directoryName: sampleFolderName)
+        XCTAssert(allFiles.contains(sampleListName))
+        
+    }
+    
+    func testCopyPlistFromBundle() {
+         let expect = expectation(description: "Plist copy from bundle")
+        let list = Plist(_name: sampleListNameBundle, _source: .bundle, _folderPath: sampleFolderName)
+         let allFiles:[String] = Plist.allPlistFiles(directoryName: sampleFolderName)
+        XCTAssert(allFiles.contains(sampleListNameBundle))
+        list.read(key: "team") { (data, error) in
+            XCTAssert(error == nil )
+            guard let theData = data else {
+                XCTAssert(data != nil)
+                return
+            }
+            guard let dct = theData["team"] as? [String:Any] , let name = dct["name"] as? String else {
+                XCTFail()
+                return
+            }
+            XCTAssert(name == "besiktas")
+            expect.fulfill()
+        }
+        
+        waitForExpectations(timeout: 4.0) { (_) -> Void in
+        }
+    }
+    
+    func testSaveDataToPlist() {
+        let expect = expectation(description: "Saving fullfill")
+        let list = Plist(_name: sampleListName , _source: .documentsDirectory, _folderPath:sampleFolderName)
+        let dataToSave:[String : Any] = ["name":"cristiano" , "surname" : "ronaldo"]
+        list.save(key: "football", data: dataToSave) { error in
+             XCTAssert(error == nil )
+            list.read(key: "football", completion: { (data, error) in
+               
+                XCTAssert(error == nil )
+                guard let theData = data else {
+                    XCTAssert(data != nil)
+                    return
+                }
+                guard let dct = theData["football"] as? [String:Any] , let name = dct["name"] as? String else {
+                    XCTFail()
+                    return
+                }
+                XCTAssert(name == "cristiano")
+                expect.fulfill()
+            })
+        }
+        
+        waitForExpectations(timeout: 4.0) { (_) -> Void in
+        }
+    }
+    
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
