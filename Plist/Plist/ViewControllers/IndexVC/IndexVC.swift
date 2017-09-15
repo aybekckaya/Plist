f98d9220df4b7e5a26663d35dcad3a385f1e345d
aybek can kaya <aybekcankaya@icloud.com>
2017-09-15 19:33:58 +0300

    before cocoapods

--- Plist/Plist/ViewControllers/IndexVC/IndexVC.swift
+++ Plist/Plist/ViewControllers/IndexVC/IndexVC.swift
@@ -14,6 +14,37 @@ class IndexVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // creating plist at /documentsFolder/plistPath/myPlist.plist
+        let list = Plist(_name: "myList", _source: .documentsDirectory, _folderPath: "plistPath")
+        
+        //copy list from bundle to documents folder
+        let listAtBundle = Plist(_name: "myListAtBundle", _source: .bundle, _folderPath: "plistPath")
+        
+        // save data to plist
+        let data:[String:Any] = ["name" : "forest" , "surname":"gump" , "id":132]
+        list.save(key: "myKey", data: data) { error in
+            print("error : \(error)")
+        }
+        
+        // read data from list
+        list.read(key: "myKey") { (data, error) in
+            guard error == nil else {
+                print("error : \(error)")
+                return
+            }
+        }
+        
+        // remove value with key
+        list.removeKey(key: "myKey")
+        
+        //remove Plist from documents directory
+        list.removePlist()
+        
+        //all plists' names from given folder at documents directory
+        let allLists = Plist.allPlistFiles(directoryName: "plistPath")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
